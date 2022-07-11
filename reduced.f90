98e2beabc45fce58cce41df241e03dad72886b4f
U-ESERVICES\kgx4xr <kgx4xr@MAE-FJZXMD2.eservices.virginia.edu>
2022-07-11 16:54:52 -0400

    interface added to extract pos, force from OUTCAR

--- reduced.f90
+++ reduced.f90
@@ -9,10 +9,18 @@ integer(8) total_atom
 real(8), allocatable :: position(:,:)
 real(8) volume
 end type lattice
-! user defined constructor
+! user defined constructor for lattice derived type
 interface lattice
   module procedure get_structure
 end interface lattice
+type vaspForce
+  integer(4) natom
+real(8), allocatable :: position(:,:), force(:,:)
+end type vaspForce
+! user defined constructor for vaspForce derived type
+interface vaspForce
+module procedure get_vaspstructure
+end interface vaspForce
 contains
   ! Routine to find a word, it could be anything
   subroutine findword(word,line,found,i)
@@ -131,15 +139,47 @@ contains
       endif
     close(posread)
   end function get_structure
+! Routine to get position and force(in any case) from VASP OUTCAR
+  function get_vaspstructure(filename) result(self)
+    type(vaspForce) :: self
+    character(8) filename*6, line*199, word*29
+    integer(4) i,j,maxl,p,outread,reason,natom
+    logical found
+    outread=456
+    maxl=9999999
+    open(outread,file=filename)
+    do i=1,maxl
+        read(outread,'(A)',iostat=reason) line
+        word='NIONS'
+        call findword(word,line,found,p)
+        if ( found ) then
+          read(line(p+12:),*) natom
+        endif
+        word=' POSITION'
+        call findword(word,line,found,p)
+        if (found) then
+          allocate(self%position(natom,3))
+          allocate(self%force(natom,3))
+          read(outread,'(A)',iostat=reason) line
+          do j=1,natom
+            read(outread,*,iostat=reason) self%position(j,:),self%force(j,:)
+          enddo
+          exit
+        endif
+    enddo
+    self%natom=natom
+    close(outread)
+  end function get_vaspstructure
 end module poscar
 
 program image
 use poscar
 type(lattice) lat
+type(vaspForce) vasp
 integer(4) num_args, p, i
 character(8) word*6
 character(8), dimension(:), allocatable :: args
-logical found
+logical found,foundP,foundPF,foundO,foundOF
 num_args=command_argument_count()
 if (num_args .ne. 2) then
   write(*,*) "Please provide POSCAR and OUTCAR file during execution"
@@ -151,38 +191,36 @@ if (num_args .eq. 2) then
     !Check if POSCAR is present and read it
     if(i .eq. 1) then
       word='POSCAR'
-      call findword(word,args(i),found,p)
-      if (.not. found) then
+      call findword(word,args(i),foundP,p)
+      if (.not. foundP) then
         write(*,*) "Provide POSCAR first as an argument"
       endif
-      if (found) then
-        inquire(file=args(i),exist=found)
-        if (.not. found) then
+      if (foundP) then
+        inquire(file=args(i),exist=foundPF)
+        if (.not. foundPF) then
           write(*,*) "POSCAR NOT FOUND IN THE CURRENT DIRECTORY"
         endif
-        if (found) then
+        if (foundPF) then
           write(*,*) "POSCAR FOUND IN THE CURRENT DIRECTORY"
           lat=get_structure(args(i))
-          deallocate(lat%atoms_type)
-          deallocate(lat%atoms_num)
-          deallocate(lat%position)
         endif
       endif
     endif
     !Check if OUTCAR is present and read it
     if(i .eq. 2) then
       word='OUTCAR'
-      call findword(word,args(i),found,p)
-      if (.not. found) then
+      call findword(word,args(i),foundO,p)
+      if (.not. foundO) then
         write(*,*) "Provide OUTCAR as second argument"
       endif
-      if (found) then
-        inquire(file=args(i),exist=found)
-        if (.not. found) then
+      if (foundO) then
+        inquire(file=args(i),exist=foundOF)
+        if (.not. foundOF) then
           write(*,*) "OUTCAR NOT FOUND IN THE CURRENT DIRECTORY"
         endif
-        if (found) then
+        if (foundOF) then
           write(*,*) "OUTCAR FOUND IN THE CURRENT DIRECTORY"
+          vasp=get_vaspstructure(args(i))
         endif
       endif
     endif
@@ -190,169 +228,13 @@ if (num_args .eq. 2) then
   enddo
   deallocate(args)
 endif
-! type(lattice) :: lat
-! integer(4) p
-! integer(8) maxl, i, j, k, posread, reason, natom, atoms_len, &
-!  space_num, atom_type, atom_number, total_atom, outread
-!  integer(8), allocatable :: atom_type_num(:)
-! real(8), allocatable:: position(:,:), pos_cart(:,:), pos_outcar(:,:), force_outcar(:,:)
-! real(8), allocatable :: pos_outcar_new(:,:), rij(:,:)
-! character line*199, word*59, space*1, atom_name*2
-! logical found
-! posread=456
-! outread=789
-! maxl=9999999
-! open(posread,file="./POSCAR")
-! read(posread,'(A)',iostat=reason) line
-! read(posread,'(A)',iostat=reason) line
-! read(posread,*,iostat=reason) lat%direct(1,1), lat%direct(1,2), lat%direct(1,3)
-! read(posread,*,iostat=reason) lat%direct(2,1), lat%direct(2,2), lat%direct(2,3)
-! read(posread,*,iostat=reason) lat%direct(3,1), lat%direct(3,2), lat%direct(3,3)
-! write(*,*) lat%direct(1,1), lat%direct(1,2), lat%direct(1,3)
-! write(*,*) lat%direct(2,1), lat%direct(2,2), lat%direct(2,3)
-! write(*,*) lat%direct(3,1), lat%direct(3,2), lat%direct(3,3)
-! ! evaluate the reciprocal lattice vector
-! lat%reciprocal(1,1)=lat%direct(2,2)*lat%direct(3,3)-lat%direct(3,2)*lat%direct(2,3)
-! lat%reciprocal(1,2)=lat%direct(3,1)*lat%direct(2,3)-lat%direct(2,1)*lat%direct(3,3)
-! lat%reciprocal(1,3)=lat%direct(2,1)*lat%direct(3,2)-lat%direct(3,1)*lat%direct(2,2)
-! !-------------------------------------------------------------------------------
-! lat%reciprocal(2,1)=lat%direct(3,2)*lat%direct(1,3)-lat%direct(3,3)*lat%direct(1,2)
-! lat%reciprocal(2,2)=lat%direct(1,1)*lat%direct(3,3)-lat%direct(3,1)*lat%direct(1,3)
-! lat%reciprocal(2,3)=lat%direct(3,1)*lat%direct(1,2)-lat%direct(1,1)*lat%direct(3,2)
-! !-------------------------------------------------------------------------------
-! lat%reciprocal(3,1)=lat%direct(1,2)*lat%direct(2,3)-lat%direct(2,2)*lat%direct(1,3)
-! lat%reciprocal(3,2)=lat%direct(2,1)*lat%direct(1,3)-lat%direct(1,1)*lat%direct(2,3)
-! lat%reciprocal(3,3)=lat%direct(1,1)*lat%direct(2,2)-lat%direct(2,1)*lat%direct(1,2)
-! lat%volume=lat%direct(1,1)*lat%reciprocal(1,1)+lat%direct(1,2)*lat%reciprocal(1,2)*&
-! lat%direct(1,3)*lat%reciprocal(1,3)
-! !-------------------------------------------------------------------------------
-! do i=1,3
-! do j=1,3
-! lat%reciprocal(i,j)=lat%reciprocal(i,j)/lat%volume
-! enddo
-! enddo
-! read(posread,'(A)',iostat=reason) line
-! write(*,*) line
-! space=' '
-! space_num=iachar(space)
-! atom_type=0
-! do i=1,len_trim(line)-1
-! if(iachar(line(i:i)) .ne. space_num .and. iachar(line(i+1:i+1)) .ne. space_num) then
-! atom_name=line(i:i+1)
-! atom_type=atom_type+1
-! write(*,*) atom_name, atom_type
-! endif
-! enddo
-! allocate(atom_type_num(atom_type))
-! read(posread,*,iostat=reason) atom_type_num(:)
-! write(*,*) atom_type_num
-! write(*,*) shape(atom_type_num)
-! total_atom=0
-! do i=1,atom_type
-! total_atom=total_atom+atom_type_num(i)
-! enddo
-! write(*,*) "total atom is: ",total_atom
-! read(posread,'(A)',iostat=reason) line
-! write(*,*) line
-! allocate(position(total_atom,3))
-! do i=1,total_atom
-! if (reason < 0) then
-! write(*,*) "END OF FILE"
-! exit
-! endif
-! read(posread,'(A)',iostat=reason) position(i,1),position(i,2),position(i,3)
-! enddo
-! close(posread)
-! ! Check for direct coordinates
-! word='D'
-! call findword(word,line,found,p)
-! if ( found ) then
-! write(*,*) "Found Direct/direct coordinates"
-! allocate(pos_cart(total_atom,3))
-! pos_cart=0.0d0
-! do i=1,total_atom
-! do j=1,3
-! pos_cart(i,1)=pos_cart(i,1)+position(i,j)*lat%direct(j,1)
-! pos_cart(i,2)=pos_cart(i,2)+position(i,j)*lat%direct(j,2)
-! pos_cart(i,3)=pos_cart(i,3)+position(i,j)*lat%direct(j,3)
-! enddo
-! enddo
-! endif
-! ! Should be noted it could be either D or d for Direct
-! word='d'
-! call findword(word,line,found,p)
-! if ( found ) then
-! write(*,*) "Found Direct/direct coordinates"
-! allocate(pos_cart(total_atom,3))
-! pos_cart=0.0d0
-! do i=1,total_atom
-! do j=1,3
-! pos_cart(i,1)=pos_cart(i,1)+position(i,j)*lat%direct(j,1)
-! pos_cart(i,2)=pos_cart(i,2)+position(i,j)*lat%direct(j,2)
-! pos_cart(i,3)=pos_cart(i,3)+position(i,j)*lat%direct(j,3)
-! enddo
-! enddo
-! endif
-! ! Read OUTCAR file beyond this point
-! allocate(pos_outcar(total_atom,3),force_outcar(total_atom,3))
-! allocate(pos_outcar_new(total_atom,3))
-! allocate(rij(total_atom,3))
-! open(outread,file="./OUTCAR")
-! do i=1,maxl
-! read(outread,'(A)',iostat=reason) line
-! word=' POSITION'
-! call findword(word,line,found,p)
-! if(found) then
-! read(outread,'(A)',iostat=reason) line
-! do j=1,total_atom
-! read(outread,*,iostat=reason) pos_outcar(j,1),pos_outcar(j,2), &
-! pos_outcar(j,3), force_outcar(j,1), force_outcar(j,2), force_outcar(j,3)
-! !write(*,*) pos_outcar(j,1),pos_outcar(j,2),pos_outcar(j,3), &
-! !force_outcar(j,1),force_outcar(j,2),force_outcar(j,3)
-! enddo
-! endif
-! if(reason < 0) then
-! write(*,*) ".......END OF OUTCAR......."
-! exit
-! endif
-! enddo
-! ! Get the distance of each atom from POSCAR and OUTCAR file
-! do i=1,total_atom
-! do j=1,3
-! rij(i,j)=pos_outcar(i,j)-pos_cart(i,j)
-! enddo
-! enddo
-! ! Try to change the cartesian coordinates from OUTCAR to reduce coordinates
-! write(*,*) "--------------------------------------------------------------------"
-! do i=1,total_atom
-! lat%a=0.0d0
-! pos_outcar_new(i,1)=0.0d0
-! pos_outcar_new(i,2)=0.0d0
-! pos_outcar_new(i,3)=0.0d0
-! do j=1,3
-! lat%a(1)=lat%a(1)+pos_outcar(i,j)*lat%reciprocal(1,j)
-! lat%a(2)=lat%a(2)+pos_outcar(i,j)*lat%reciprocal(2,j)
-! lat%a(3)=lat%a(3)+pos_outcar(i,j)*lat%reciprocal(3,j)
-! enddo
-! write(*,*) lat%a(1),lat%a(2),lat%a(3)
-! lat%a(1)=lat%a(1)-nint(lat%a(1))
-! lat%a(2)=lat%a(2)-nint(lat%a(2))
-! lat%a(3)=lat%a(3)-nint(lat%a(3))
-! write(*,*) lat%a(1),lat%a(2),lat%a(3)
-! !lat%a(1)=anint(lat%a(1))
-! !lat%a(2)=anint(lat%a(2))
-! !lat%a(3)=anint(lat%a(3))
-! write(*,*) position(i,1), position(i,2), position(i,3)
-! write(*,*) "-------------------------------------------------------------------"
-! do j=1,3
-! pos_outcar_new(i,j)=lat%a(1)*lat%direct(1,j)+lat%a(2)*lat%direct(2,j)+ &
-! lat%a(3)*lat%direct(3,j)
-! enddo
-! !write(*,*) lat%a(1),lat%a(2),lat%a(3)
-! enddo
-! close(outread)
-! write(*,*) "--------------------------------------------------------------------"
-! write(*,*) position(200,3)
-! deallocate(atom_type_num,position,pos_cart)
-! deallocate(pos_outcar,force_outcar,pos_outcar_new)
+if (num_args .eq. 2) then
+  if(foundP .and. foundPF) then
+    deallocate(lat%atoms_type)
+    deallocate(lat%atoms_num)
+    deallocate(lat%position)
+    deallocate(vasp%position)
+    deallocate(vasp%force)
+  endif
+endif
 end program image
